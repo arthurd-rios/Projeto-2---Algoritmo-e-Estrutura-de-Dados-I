f5db219fab9a3b1ace80e64d37ae7ea591a7df2a
arthurd-rios <arthurd.rios10@gmail.com>
2024-12-10 18:07:43 -0300

    Update main.c

--- main.c
+++ main.c
@@ -7,25 +7,14 @@ int main(){
     CONJ *A, *B;
     int tipoed, a_tam, b_tam, elemento, operacao;
 
-    printf("Digite as estrutura de dados que será utilizada: \n");
-    printf("0 - Árvore AVL\n");
-    printf("1 - Árvore Rubro-Negra\n");
-    printf("\n");
-
     scanf("%d", &tipoed);
 
     A = conjunto_criar(tipoed);
     B = conjunto_criar(tipoed);
 
-    printf("Digite o tamanho do conjunto A: \n");
     scanf("%d", &a_tam);
 
-    printf("Digite o tamanho do conjunto B: \n");
     scanf("%d", &b_tam);
-
-    printf("\n");
-
-    printf("Insira os %d elementos do conjunto A: \n", a_tam);
 
     for(int i = 0; i < a_tam; i++){
 
@@ -34,21 +23,12 @@ int main(){
 
     }
 
-    printf("Insira os %d elementos do conjunto B: \n", b_tam);
-
     for(int i = 0; i < b_tam; i++){
 
         scanf("%d", &elemento);
         conjunto_inserir(B, elemento);
 
     }
-
-    printf("Digite a operação que será feita: \n");
-    printf("1 - Interseção entre os dois conjuntos\n");
-    printf("2 - União dos dois conjuntos\n");
-    printf("3 - Conferir se um elemento pertence ao conjunto A\n");
-    printf("4 - Remover um elemento do conjunto A\n");
-    printf("\n"); 
 
     scanf("%d", &operacao);
 
@@ -58,8 +38,10 @@ int main(){
 
         C = conjunto_intersecao(A, B);
 
-        printf("A interseção entre os conjuntos A e B é: ");
+
         conjunto_imprimir(C);
+
+        conjunto_apagar(&C);
 
     }
 
@@ -69,49 +51,45 @@ int main(){
 
         C = conjunto_uniao(A, B);
 
-        printf("A união dos conjuntos A e B é: ");
         conjunto_imprimir(C);
         printf("\n");
+
+        conjunto_apagar(&C);
 
     }
 
     if (operacao == 3){
 
-        printf("Digite o elemento: \n");
-
         scanf("%d", &elemento);
 
         if(conjunto_pertence(A, elemento))
-            printf("Pertence\n");
+            printf("Pertence.");
 
         else
-            printf("Nao Pertence\n");
+            printf("Nao Pertence.");
 
     }
 
     if (operacao == 4){
 
-        printf("Digite o elemento: \n");
-
         scanf("%d", &elemento);
 
-        if(conjunto_pertence(A, elemento)){
-
+        if(conjunto_pertence(A, elemento))
             conjunto_remover(A, elemento);
-            
-            printf("Conjunto A sem o elemento %d: ", elemento);
-            conjunto_imprimir(A);
-
-        }
 
         else
-            printf("Elemento %d não encontrado no conjunto A\n", elemento);
+            printf("Elemento nao encontrado.");
+
+        conjunto_imprimir(A);
+        
 
     }
 
-    if (operacao > 4 || operacao < 1)
-        printf("Operação Inválida");
+    if(operacao < 1 || operacao > 4)
+        printf("Operacao Invalida");
         
+    conjunto_apagar(&A);
+    conjunto_apagar(&B);
 
     return 0;
 
